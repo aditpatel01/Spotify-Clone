ab17fe4ddc6e2615baab1fdb3f8b5cfe474512c0
Adit Patel <pateladit2001@gmail.com>
2021-05-26 23:33:34 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+const express = require('express')
+const SpotifyWebApi = require('spotify-web-api-node')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const lyricsFinder = require('lyrics-finder')
+
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post("/refresh",(req, res)=>{
+    const refreshToken = req.body.refreshToken
+    const credentials = {
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        redirectUri: process.env.REDIRECT_URI,
+        refreshToken
+    }
+    const spotifyApi = new SpotifyWebApi(credentials)
+    spotifyApi.refreshAccessToken().then(
+        (data)=> {
+          res.json({
+              accessToken: data.body.accessToken,
+              expiresIn: data.body.expiresIn
+          })
+        }).catch(()=>{
+            res.sendStatus(400)
+        })
+        
+})
+
+app.post("/login", (req, res)=>{
+    const code = req.body.code
+    const credentials = {
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        redirectUri: process.env.REDIRECT_URI
+    }
+    const spotifyApi = new SpotifyWebApi(credentials)
+
+    spotifyApi.authorizationCodeGrant(code).then((data)=>{
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in
+            })
+        }
+    ).catch(()=>{
+        res.sendStatus(400)
+    })
+
+})
+
+app.get("/lyrics", async(req, res)=>{
+    const lyrics = await lyricsFinder(req.query.artist, req.query.track)|| "No Lyrics Found!"
+    res.json({lyrics})
+})
+
+app.listen(5000)
